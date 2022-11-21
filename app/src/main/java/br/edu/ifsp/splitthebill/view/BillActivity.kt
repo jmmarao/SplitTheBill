d660b9e497c64558395b4173c0256de1502a96f2
jmmarao <jmmarao@gmail.com>
2022-11-21 17:29:20 -0300

    Activity and tile layout implemented

---
+++ app/src/main/java/br/edu/ifsp/splitthebill/view/BillActivity.kt
@@ -0,0 +1,12 @@
+package br.edu.ifsp.splitthebill.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.edu.ifsp.splitthebill.R
+
+class BillActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bill)
+    }
+}
